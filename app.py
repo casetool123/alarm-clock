b94c932e64c9546e9bc85a30f193ac1d465d6862
casetool123 <amritaghosh712000@gmail.com>
2021-04-25 17:41:40 +0530

    cleaning code

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import pywhatkit
 
 app = Flask(__name__)
 
-# @app.route('/')
 @app.route('/', methods=['GET','POST'])
 def set_alarm():
     if request.method == "POST":
@@ -19,28 +18,16 @@ def set_alarm():
         set_time = str(request.form.get('set_time'))
         print("set_time is", set_time," ", type(set_time))
 
-        # print("time.localtime() is ",time.localtime()," ", type(time.localtime()))
         print("datetime.date.today() is " , datetime.date.today(), " ", type(datetime.date.today))
         
-        # print(curr_date)
-
-        
-        
-        # print(curr_time)
-        # print(set_time)
-        # print(curr_date)
-        # print(schedule_date)
 
         while True:
             curr_date = str(datetime.date.today())
             now = datetime.datetime.now()
             hour = str(now.hour)
             minute = str(now.minute)
-            # seconds = str(now.second)
             curr_time = hour + ":" + minute 
-            # print("Current time is: "  + curr_time)
             if (curr_time == set_time) and  (curr_date == schedule_date):
-                # print(f.renderText("BEEP BEEP IT'S TIME...  "))
                 pywhatkit.playonyt("programming in python")
                 time.sleep(60)
 
